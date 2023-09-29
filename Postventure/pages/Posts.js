70712ddba129138f58342c62a0bea4d83fd5b931
João Victor Almeida Rodrigues <109368541+DoomerO@users.noreply.github.com>
2023-09-29 08:26:40 -0300

    Ohhh

--- Postventure/pages/Posts.js
+++ Postventure/pages/Posts.js
@@ -1,4 +1,4 @@
-import {View, Box, Flex, Stack, Input, Text, InputGroup, InputRightAddon} from "native-base"
+import {View, Box, Flex, Stack, Input, Text, InputGroup, InputRightAddon, ScrollView} from "native-base"
 import Icon from "react-native-vector-icons/Ionicons";
 import { useEffect, useState } from "react";
 import { TouchableHighlight } from "react-native-web";
@@ -36,33 +36,36 @@ const Posts = () => {
     function postTest() {
         socket.emit("makePost", post)
         
+        //setPosts((prev) => ([...prev, {name : post.name , content : post.content}]))
         setPost((prev) => ({...prev, content : ""}));
     }
 
     const renderPosts = postsArray.map((item, index) => {
         return <Stack key={item.id} space={2} direction="column" w="90%" h="fit-content">
-            <Text display={(index > 0) ? (item.name == postsArray[index - 1].name) ? "none" : "normal" : "normal"} fontWeight="semibold" fontSize="25px" color={(item.name == post.name) ? "#a2d3f5" : "#f5d3a2"} borderBottomColor="#fff">{item.name + " >"}</Text>
-            <Text textAlign="justify" color="#fff" fontSize="22px">{item.content}</Text>
+            <Text display={(index > 0) ? (item.name == postsArray[index - 1].name) ? "none" : "normal" : "normal"} fontWeight="semibold" fontSize="20px" color={(item.name == post.name) ? "#a2d3f5" : "#f5d3a2"} borderBottomColor="#fff">{item.name + " >"}</Text>
+            <Text textAlign="justify" color="#fff" fontSize="17px">{item.content}</Text>
         </Stack>
     })
 
     return (
         <View w="100%" h="100%" bg="#14181f">
             <Box w="100%" h="100%" display="flex" flexDirection="column" alignItems="center">
-                <Stack space={2} w="90%" pt="2vh" maxH="65%" minH="65%" overflowY="auto">
-                    {renderPosts}
+                <Stack space={0.12} w="90%" pt="2vh" maxH="65%" minH="65%" overflowY="auto">
+                    <ScrollView>
+                        {renderPosts}
+                    </ScrollView>
                 </Stack>
                 <Stack space={4} h="30%" w="100%" alignItems="center" pt="20px">
                     <Flex direction="column">
                         <Text color="#fff" fontWeight="semibold" fontSize="22px">Postar como:</Text>
-                        <Input type="text" placeholder="Nome" color="#fff" fontSize="18px" value={post.name} name="name" onChangeText={(text) => {setPost(prev => ({...prev, name: text}))}} w="80vw"/>
+                        <Input type="text" placeholder="Nome" color="#fff" fontSize="18px" value={post.name} name="name" onChangeText={(text) => {setPost(prev => ({...prev, name: text}))}} w="95%"/>
                     </Flex>
                     <Flex direction="column">
                         <Text color="#fff" fontWeight="semibold" fontSize="22px">Mensagem:</Text>
                         <InputGroup>
-                            <Input type="text" placeholder="Escreva aqui" fontSize="18px" value={post.content} onChangeText={(text) => {setPost(prev => ({...prev, content: text}))}} name="content" color="#fff" w="70vw"/>
+                            <Input type="text" placeholder="Escreva aqui" fontSize="18px" value={post.content} onChangeText={(text) => {setPost(prev => ({...prev, content: text}))}} name="content" color="#fff" w="80"/>
                             <TouchableOpacity onPress={postTest}>
-                                <InputRightAddon w="10vw" h="100%"><Icon name="send" size="100%"/></InputRightAddon>
+                                <InputRightAddon w="20" h="12"><Icon name="send" size="20"/></InputRightAddon>
                             </TouchableOpacity>
                         </InputGroup>
                     </Flex>
--- Postventure/pages/Splash.js
+++ Postventure/pages/Splash.js
@@ -1,19 +1,20 @@
 import {View, Box, Text, Image} from "native-base";
-import PostVentureLogo from "../img/Postventure.png";
 import { useEffect } from "react";
 
 const Splash = ({ navigation }) => {
     
+    const post = require('../img/Postventure.png')
+
     useEffect(() => {
         setTimeout(() => {
             navigation.replace('Posts'); 
-        }, 1000); 
+        }, 3000); 
     }, []);
 
     return (
         <View w="100%" h="100%" bg="#14181f">
             <Box w="100%" h="100%" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                <Image src={PostVentureLogo} size="xl"></Image>
+                <Image source={post} size="xl" w="200" alt="Logo"></Image>
                 <Text color="#fff" fontSize="25px" fontWeight="semibold" letterSpacing="0.5px">Postventure</Text>
             </Box>
         </View>
